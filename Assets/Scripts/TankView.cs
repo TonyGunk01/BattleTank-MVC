740cefa5695f0cee3286b83c0163d9cfb7e6d504
TonyGunk01 <rishivl@hotmail.com>
2025-12-06 17:58:04 +0530

    tank movement setup
    adding rigidbody and box collider for Tank prefab
    
    accepting user input through WASD to move and rotate tank

--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -6,6 +6,11 @@ public class TankView : MonoBehaviour
 {
     private TankController tankController;
 
+    private float movement;
+    private float rotation;
+
+    public Rigidbody rb;
+
     void Start()
     {
         
@@ -13,11 +18,28 @@ public class TankView : MonoBehaviour
 
     void Update()
     {
-        
+        Movement();
+
+        if (movement != 0)
+            tankController.Move(movement, 30);
+
+        if (rotation != 0)
+            tankController.Rotate(rotation, 20);
+    }
+
+    private void Movement()
+    {
+        movement = Input.GetAxis("Vertical");
+        rotation = Input.GetAxis("Horizontal");
     }
 
     public void setTankController(TankController tank_controller)
     {
         tankController = tank_controller;
+    }
+
+    public Rigidbody getRigidbody()
+    {
+        return rb;
     }
 }
